4bda931e172850564e24dbb5e78bb196f3409078
hamza.belyahiaoui <hamza.belyahiaoui@spie.com>
2025-09-22 10:29:17 +0200

    fix gitignore

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -17,6 +17,24 @@ resource "azurerm_mysql_flexible_server" "db" {
 }
 
 
+resource "azurerm_mysql_flexible_database" "default" {
+  name                = "prestashop"
+  resource_group_name = azurerm_mysql_flexible_server.db.resource_group_name
+  server_name         = azurerm_mysql_flexible_server.db.name
+  charset             = "utf8mb4"
+  collation           = "utf8mb4_unicode_ci"
+}
+
+resource "azurerm_mysql_flexible_server_firewall_rule" "allow_all" {
+  name                = "AllowAll"
+  resource_group_name = azurerm_mysql_flexible_server.db.resource_group_name
+  server_name         = azurerm_mysql_flexible_server.db.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
+}
+
+
+
 /*mysql_sku_name = "B_Standard_B1s"
 mysql_storage_mb = 51200
 mysql_backup_retention_days = 7*/
--- modules/database/v.tf
+++ modules/database/v.tf
@@ -2,8 +2,13 @@ variable "location" {
   default = "francecentral"
 }
 variable "resource_group_name" {}
-variable "admin_user" {}
-variable "admin_password" {}
+variable "admin_user" {
+  default = "hamzauser"
+}
+variable "admin_password" {
+  default = "hamzapassword"
+
+}
 variable "sku_name" {
   default = "Standard_B1ms"
 }
